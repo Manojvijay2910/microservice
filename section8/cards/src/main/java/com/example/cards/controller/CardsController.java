4d06959d6d5766db839c37c26242a5d699a4a7b8
Manoj Vijayvergiya <manojvijay29@gmail.com>
2026-07-14 19:00:45 +0530

    added yaml files for observability with help of grafana loki, premeteus and tempo

--- section8/cards/src/main/java/com/example/cards/controller/CardsController.java
+++ section8/cards/src/main/java/com/example/cards/controller/CardsController.java
@@ -84,8 +84,9 @@ public class CardsController {
                             @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                             String mobileNumber) {
 
-        logger.debug("correlation-id found: {}", correlationId);
+        logger.debug("fetchCardDetails method start");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        logger.debug("fetchCardDetails method end");
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(cardsDto);
