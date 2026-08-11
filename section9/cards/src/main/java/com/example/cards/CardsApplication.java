1535b12c0aae142359c4763812867758cf996ff4
Manoj Vijayvergiya <manojvijay29@gmail.com>
2026-08-12 00:37:28 +0530

    Added Kubernetes and kubernetes server instead of eurekaserver

--- section9/cards/src/main/java/com/example/cards/CardsApplication.java
+++ section9/cards/src/main/java/com/example/cards/CardsApplication.java
@@ -9,9 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 /*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
 @EnableJpaRepositories("com.eazybytes.cards.repository")
 @EntityScan("com.eazybytes.cards.model")*/
